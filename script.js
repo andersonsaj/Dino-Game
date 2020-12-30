ef53a22594c60cdfae4f48f501df809b33d134a4
andersonsaj <andersonsb09@hotmail.com>
2020-12-30 18:50:40 -0300

    criado a movimentação do cactus e a geração de vários cactus

--- script.js
+++ script.js
@@ -15,21 +15,23 @@ function jump() {
 
     isJumping = true;
   
+    //subindo
     let upInterval = setInterval(() => {
         if (position >= 170) {
             clearInterval(upInterval);
-
+            //descendo
             let downInternal = setInterval(() => {
                 if (position <= 20) { 
                     clearInterval(downInternal);
                     isJumping = false;
                 }else{
+
                     position -= 20;
                     dino.style.bottom = position + 'px';
                 }
             })
         }else {
-            //subindo
+            
             position += 20;
             dino.style.bottom = position + 'px';
         }
@@ -38,12 +40,24 @@ function jump() {
 
 function createCactus() {
     const cactus = document.createElement('div');
-    let cactusPosition = 1000;
+    let cactusPosition = 2000;
+    let randomTime = Math.random() * 6000;
 
     cactus.classList.add('cactus');
-    cactus.style.left = 1000 + 'px';
+    cactus.style.left = 2000 + 'px';
     background.appendChild(cactus);
 
+    let leftInternal = setInterval(() => {
+        if (cactusPosition < -60) {
+            clearInterval(leftInternal);
+            background.removeChild(cactus);
+        } else {
+            cactusPosition -= 10;
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+    setTimeout(createCactus, randomTime);
 }
 
 createCactus();
